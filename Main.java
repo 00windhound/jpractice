a00f7bae2bd4eaa1d84572a1df1e7c3dfa515ade
crystalyn <00crystalynthompson@gmail.com>
2024-01-03 20:52:14 -0800

    adding stuff to frame

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main{
 public static void main (String[]args){
@@ -21,8 +22,12 @@ frame.setResizable(false);
 frame.setSize(1000, 1000);
 
 ImageIcon icon =new ImageIcon("doodle.png");
+JLabel label =new JLabel();
+label.setText("hello");
 frame.setIconImage(icon.getImage());
 frame.getContentPane().setBackground(new Color(0x123456));
-
+frame.add(label);
+ImageIcon image = new ImageIcon("doodle.png");
+label.setIcon(image);
 }
 }
